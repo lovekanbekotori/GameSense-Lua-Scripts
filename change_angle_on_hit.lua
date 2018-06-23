7b5a93a6d430c6f3eb4567f21c77f24258a6e549
Matthew <warmestyogurt@gmail.com>
2018-06-23 13:37:22 -0700

    isa p

--- change_angle_on_hit.lua
+++ change_angle_on_hit.lua
@@ -1,5 +1,3 @@
-local notifications = require("notifications");
-
 local toggles = {};
 toggles.enabled = ui.new_checkbox("AA", "Anti-aimbot angles", 
     "Change angle on hit");
@@ -8,45 +6,100 @@ local sliders = {};
 sliders.reset_time = ui.new_slider("AA", "Anti-aimbot angles", "Reset time", 1,
     10, 1, true, "s");
 
+local refs = {};
+refs.yaw = ui.reference("AA", "Anti-aimbot angles", "Yaw");
+
 -- set defaults
 ui.set(toggles.enabled, false);
 ui.set(sliders.reset_time, 1);
 
 local antiaims = {
-    {
-        name: "Sideways"
-    },
-    {
-        name: "180 Z"
-    },
-    {
-        name: "180"
-    }
+    "Sideways", "180 Z", "180"
 };
+-- used when all antiaims are blacklisted
+-- set to nil if you want to set it to
+-- a random blacklisted angle
+local fallback_antiaim = nil;
 
-local function get_local_player() then
+local cur_antiaim = antiaims[1];
+local hit_antiaims = {};
+local last_hit_time = 0;
+
+local function get_local_player()
     local local_player = entity.get_local_player();
 
-    if (entity.get_player_weapon(local_player)) then
+    if entity.get_player_weapon(local_player) then
         return local_player
     else
         return nil;
     end
 end
 
+local function is_blacklisted(antiaim)
+    local is_blacklisted = false;
+    for _, val in pairs(hit_antiaims) do
+        if val == antiaim then
+            is_blacklisted = true;
+        end
+    end
+    return is_blacklisted;
+end
+
+local function reset_antiaim()
+    if cur_antiaim ~= antiaims[1] then
+        client.log("resetting");
+        cur_antiaim = antiaims[1];
+        ui.set(refs.yaw, cur_antiaim);
+    end
+    for idx, _ in pairs(hit_antiaims) do
+        table.remove(hit_antiaims, idx);
+    end
+    last_hit_time = 0;
+end
+
+local function change_antiaim()
+    for _, val in pairs(antiaims) do
+        if not is_blacklisted(val) then
+            client.log(val .. " isn't blacklisted");
+            --client.log(val);
+            cur_antiaim = val;
+            ui.set(refs.yaw, cur_antiaim);
+            return;
+        end
+    end
+    
+    if fallback_antiaim ~= nil then
+        ui.set(refs.yaw, fallback_antiaim);
+    else
+        ui.set(refs.yaw, hit_antiaims[client.random_int(1, #hit_antiaims)]);
+    end
+end
+
 local function on_player_hurt(e)
-    if (not ui.get(toggles.enabled)) then
+    if not ui.get(toggles.enabled) then
         return;
     end
 
-    if (not local_player) then
+    local local_player = get_local_player();
+
+    if not local_player then
         return;
     end
 
     local victim_idx = client.userid_to_entindex(e.userid);
-    if (victim_idx == local_player) then
+    if victim_idx == local_player and e.health > 0 then
+        local hit_antiaim = ui.get(refs.yaw);
+        table.insert(hit_antiaims, hit_antiaim);
+        last_hit_time = globals.curtime();
+        change_antiaim();
+    end
+end
 
+local function on_paint(ctx)
+    if globals.curtime() - last_hit_time > ui.get(sliders.reset_time) then
+        reset_antiaim();
     end
 end
 
 client.set_event_callback("player_hurt", on_player_hurt);
+client.set_event_callback("paint", on_paint);
